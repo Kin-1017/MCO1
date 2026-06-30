339dc414b54d57c166215c2d90d2c5f7ede76be9
Kin-1017 <joaquin_macaraig@dlsu.edu.ph>
2026-06-30 17:04:36 +0800

    updated

--- MCO1/src/ItemSlot.java
+++ MCO1/src/ItemSlot.java
@@ -4,8 +4,26 @@ public class ItemSlot {
     private int quantity;
     private final int capacity = 10;
 
-    public ItemSlot (int slotNumber, int quantity, boolean available) {
+    public ItemSlot (int slotNumber, Item item, int quantity) {
+        this.item = item;
         this.slotNumber = slotNumber;
         this.quantity = quantity;
     }
+
+    public boolean isAvailable () {
+        boolean available = false;
+        if (quantity < capacity) {
+            return available;
+        }
+
+        else {
+            return !available;
+        }
+    }
+
+    public void addStock (int qty) {
+
+    }
+
+
 }
